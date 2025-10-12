4e0d2314d3d086de696e27e00a9dafb2eb3c0789
Donald Sullivan <donald@fortcode.com>
2025-10-12 17:12:48 -0500

    update for vscode

--- src/WalterModem.h
+++ src/WalterModem.h
@@ -400,7 +400,7 @@ CONFIG_UINT8(SPI_SECTORS_PER_BLOCK, 16)
 #include <esp_partition.h>
 #include <esp_vfs.h>
 #include <esp_vfs_fat.h>
-#include "wm_compat_spi_flash.h"
+#include <esp_spi_flash.h>
 
 #endif
 
